9c2543ba853890528ddfc7407c6de5fdd60cd4c7
wongjenn <wongjenn@yahoo.com>
2017-01-17 16:34:22 -0500

    Done.

---
+++ index.js
@@ -0,0 +1,23 @@
+function theTruth(){
+  return true
+}
+
+function sayHey(){
+  return "Hey!"
+}
+
+function sayHeyFriend(name){
+  return `Hey, ${name}!`
+}
+
+function favoriteIceCream(){
+  return "I love mint chocolate chip"
+}
+
+function shouting(){
+  return 'I LOVE JAVASCRIPT SOOOOOO MUCH'
+}
+
+function roundDown(){
+  return 5
+}
